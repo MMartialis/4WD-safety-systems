157e84bb1576459f8b1593a64a43ac5685b1b704
volubepossum <domj.roli@gmail.com>
2023-11-14 16:51:24 +0100

    create merge hell for  mrton rework buffer change to multicore things

--- src/vesc.cpp
+++ src/vesc.cpp
@@ -2,216 +2,72 @@
 
 #include <stdint.h>
 #include <SPI.h>
+#include <algorithm>
 #include "mcp_can.h"
 #include "vesc.hpp"
 
 
-MCP_CAN CAN0(5);
+extern MCP_CAN CAN0;
+
+extern TaskHandle_t Handler0;
+extern uint8_t msgCount;
+
 long unsigned int rxId;
-extern uint8_t len;
+uint8_t len = 0;
 uint8_t rxBuf[8];
-char msgString[RX_MSG_BUFFER_LEN][128]; // Array to store serial string
-extern TaskHandle_t fasz;
-extern uint8_t ize;
+char msgBuffer[RX_MSG_BUFFER_LEN][11]; // [command id, rxId, len, data1, data2, data3, data4, data5, data6, data7, data8]
 
-// Implementation for sending extended ID CAN-frames
-void can_transmit_eid(uint32_t id, const uint8_t *data, uint8_t len, uint8_t rtr)
+// void print_raw_can_data(void *params)
+// {
+    
+//     uint8_t counter = 1;
+//     for (;CAN0.readMsgBuf(&rxId, &len, rxBuf) != CAN_NOMSG;)
+//     {
+//         sprintf(msgString, "buffer %d Standard ID: 0x%.3lX       DLC: %d  Data:", counter, rxId, len);
+//         for (byte i = 0; i < len; i++)
+//         {
+//             sprintf(msgString + strlen(msgString), " 0x%.2X", rxBuf[i]);
+//         }
+
+//         // Serial.println(msgString);
+//         ize ++;
+//         counter --;
+//     }
+//     vTaskDelete(Handler0);
+// }
+
+void core_0_setup(void *params)
 {
-    portDISABLE_INTERRUPTS();
-    CAN0.sendMsgBuf((unsigned long)id, (byte)1, (byte)rtr, (byte)len, (byte *)data);
-    portENABLE_INTERRUPTS();
+    pinMode(CAN0_INT, INPUT);
+    attachInterrupt(digitalPinToInterrupt(CAN0_INT), put_message_in_buffer, FALLING);
+    Serial.println("Interrupt attached");
+    vTaskDelete(Handler0);
 }
 
-void buffer_append_int16(uint8_t *buffer, int16_t number, int32_t *index)
+void put_message_in_buffer()
 {
-    buffer[(*index)++] = number >> 8;
-    buffer[(*index)++] = number;
-}
-
-void buffer_append_int32(uint8_t *buffer, int32_t number, int32_t *index)
-{
-    buffer[(*index)++] = number >> 24;
-    buffer[(*index)++] = number >> 16;
-    buffer[(*index)++] = number >> 8;
-    buffer[(*index)++] = number;
-}
-
-void buffer_append_float16(uint8_t *buffer, float number, float scale, int32_t *index)
-{
-    buffer_append_int16(buffer, (int16_t)(number * scale), index);
-}
-
-void buffer_append_float32(uint8_t *buffer, float number, float scale, int32_t *index)
-{
-    buffer_append_int32(buffer, (int32_t)(number * scale), index);
-}
-
-void comm_can_set_duty(uint8_t controller_id, float duty)
-{
-    int32_t send_index = 0;
-    uint8_t buffer[4];
-    buffer_append_int32(buffer, (int32_t)(duty * 100000.0), &send_index);
-    can_transmit_eid(controller_id |
-                         ((uint32_t)CAN_PACKET_SET_DUTY << 8),
-                     buffer, send_index, 0);
-}
-
-void comm_can_set_current(uint8_t controller_id, float current)
-{
-    int32_t send_index = 0;
-    uint8_t buffer[4];
-    buffer_append_int32(buffer, (int32_t)(current * 1000.0), &send_index);
-    can_transmit_eid(controller_id |
-                         ((uint32_t)CAN_PACKET_SET_CURRENT << 8),
-                     buffer, send_index, 0);
-}
-
-void comm_can_set_current_off_delay(uint8_t controller_id, float current, float off_delay)
-{
-    int32_t send_index = 0;
-    uint8_t buffer[6];
-    buffer_append_int32(buffer, (int32_t)(current * 1000.0), &send_index);
-    buffer_append_float16(buffer, off_delay, 1e3, &send_index);
-    can_transmit_eid(controller_id |
-                         ((uint32_t)CAN_PACKET_SET_CURRENT << 8),
-                     buffer, send_index, 0);
-}
-
-void comm_can_set_current_brake(uint8_t controller_id, float current)
-{
-    int32_t send_index = 0;
-    uint8_t buffer[4];
-    buffer_append_int32(buffer, (int32_t)(current * 1000.0), &send_index);
-    can_transmit_eid(controller_id |
-                         ((uint32_t)CAN_PACKET_SET_CURRENT_BRAKE << 8),
-                     buffer, send_index, 0);
-}
-
-void comm_can_set_rpm(uint8_t controller_id, float rpm)
-{
-    int32_t send_index = 0;
-    uint8_t buffer[4];
-    buffer_append_int32(buffer, (int32_t)rpm, &send_index);
-    can_transmit_eid(controller_id |
-                         ((uint32_t)CAN_PACKET_SET_RPM << 8),
-                     buffer, send_index, 0);
-}
-
-void comm_can_set_pos(uint8_t controller_id, float pos)
-{
-    int32_t send_index = 0;
-    uint8_t buffer[4];
-    buffer_append_int32(buffer, (int32_t)(pos * 1000000.0), &send_index);
-    can_transmit_eid(controller_id |
-                         ((uint32_t)CAN_PACKET_SET_POS << 8),
-                     buffer, send_index, 0);
-}
-
-void comm_can_set_current_rel(uint8_t controller_id, float current_rel)
-{
-    int32_t send_index = 0;
-    uint8_t buffer[4];
-    buffer_append_float32(buffer, current_rel, 1e5, &send_index);
-    can_transmit_eid(controller_id |
-                         ((uint32_t)CAN_PACKET_SET_CURRENT_REL << 8),
-                     buffer, send_index, 0);
-}
-
-/**
- * Same as above, but also sets the off delay. Note that this command uses 6 bytes now. The off delay is useful to set to keep the current controller running for a while even after setting currents below the minimum current.
- */
-void comm_can_set_current_rel_off_delay(uint8_t controller_id, float current_rel, float off_delay)
-{
-    int32_t send_index = 0;
-    uint8_t buffer[6];
-    buffer_append_float32(buffer, current_rel, 1e5, &send_index);
-    buffer_append_float16(buffer, off_delay, 1e3, &send_index);
-    can_transmit_eid(controller_id |
-                         ((uint32_t)CAN_PACKET_SET_CURRENT_REL << 8),
-                     buffer, send_index, 0);
-}
-
-void comm_can_set_current_brake_rel(uint8_t controller_id, float current_rel)
-{
-    int32_t send_index = 0;
-    uint8_t buffer[4];
-    buffer_append_float32(buffer, current_rel, 1e5, &send_index);
-    can_transmit_eid(controller_id |
-                         ((uint32_t)CAN_PACKET_SET_CURRENT_BRAKE_REL << 8),
-                     buffer, send_index, 0);
-}
-
-void comm_can_set_handbrake(uint8_t controller_id, float current)
-{
-    int32_t send_index = 0;
-    uint8_t buffer[4];
-    buffer_append_float32(buffer, current, 1e3, &send_index);
-    can_transmit_eid(controller_id |
-                         ((uint32_t)CAN_PACKET_SET_CURRENT_HANDBRAKE << 8),
-                     buffer, send_index, 0);
-}
-
-void comm_can_set_handbrake_rel(uint8_t controller_id, float current_rel)
-{
-    int32_t send_index = 0;
-    uint8_t buffer[4];
-    buffer_append_float32(buffer, current_rel, 1e5, &send_index);
-    can_transmit_eid(controller_id |
-                         ((uint32_t)CAN_PACKET_SET_CURRENT_HANDBRAKE_REL << 8),
-                     buffer, send_index, 0);
-}
-
-void comm_can_status_1(uint8_t controller_id)
-{
-    uint8_t buffer[0];
-    can_transmit_eid(controller_id | ((uint32_t)CAN_PACKET_STATUS_1 << 8), buffer, 0, 1);
-}
-
-void comm_can_status_2(uint8_t controller_id)
-{
-    uint8_t buffer[0];
-    can_transmit_eid(controller_id | ((uint32_t)CAN_PACKET_STATUS_2 << 8), buffer, 0, 1);
-}
-
-void comm_can_status_3(uint8_t controller_id)
-{
-    uint8_t buffer[0];
-    can_transmit_eid(controller_id | ((uint32_t)CAN_PACKET_STATUS_3 << 8), buffer, 0, 1);
-}
-
-void comm_can_status_4(uint8_t controller_id)
-{
-    uint8_t buffer[0];
-    can_transmit_eid(controller_id | ((uint32_t)CAN_PACKET_STATUS_4 << 8), buffer, 0, 1);
-}
-
-void comm_can_status_5(uint8_t controller_id)
-{
-    uint8_t buffer[0];
-    can_transmit_eid(controller_id | ((uint32_t)CAN_PACKET_STATUS_5 << 8), buffer, 0, 1);
-}
-
-void comm_can_status_6(uint8_t controller_id)
-{
-    uint8_t buffer[0];
-    can_transmit_eid(controller_id | ((uint32_t)CAN_PACKET_STATUS_6 << 8), buffer, 0, 1);
-}
-
-void print_raw_can_data(void *penis)
-{
-    Serial.println("listening");
-    uint8_t counter = 1;
-    // for (;CAN0.readMsgBuf(&rxId, &len, rxBuf) != CAN_NOMSG;)
+  for (; CAN0.readMsgBuf(&rxId, &len, rxBuf) != CAN_NOMSG;)
+  {
+    const char message[11] = {
+        (byte) rxId >> 8,
+        (byte) rxId << 24,
+        (byte) len,
+        rxBuf[0],
+        rxBuf[1],
+        rxBuf[2],
+        rxBuf[3],
+        rxBuf[4],
+        rxBuf[5],
+        rxBuf[6],
+        rxBuf[7]
+    };
+    std::copy(message, message + 11, msgBuffer[msgCount%RX_MSG_BUFFER_LEN]);
+    // uint8_t msgId = msgCount%RX_MSG_BUFFER_LEN;
+    // sprintf(msgBuffer[msgId], "buffer %d Standard ID: 0x%.3lX       DLC: %d  Data:", counter, rxId, len);
+    // for (byte i = 0; i < len; i++)
     // {
-    //     sprintf(msgString, "buffer %d Standard ID: 0x%.3lX       DLC: %d  Data:", counter, rxId, len);
-    //     for (byte i = 0; i < len; i++)
-    //     {
-    //         sprintf(msgString + strlen(msgString), " 0x%.2X", rxBuf[i]);
-    //     }     
-        
-    //     // Serial.println(msgString);
-    //     ize ++;
-    //     counter --;
+    //   sprintf(msgBuffer[msgId] + strlen(msgBuffer[msgId]), " 0x%.2X", rxBuf[i]);
     // }
-    vTaskDelete(fasz);
+  }
 }
+
--- src/vesc.hpp
+++ src/vesc.hpp
@@ -9,68 +9,8 @@ extern unsigned char len;
 extern unsigned char rxBuf[8];
 extern char msgString[RX_MSG_BUFFER_LEN][128];// Array to store serial string
 
-void can_transmit_eid(uint32_t id, const uint8_t *data, uint8_t len);
+// void print_raw_can_data(void* params);
 
-typedef enum
-{
-    CAN_PACKET_SET_DUTY = 0,
-    CAN_PACKET_SET_CURRENT = 1,
-    CAN_PACKET_SET_CURRENT_BRAKE = 2,
-    CAN_PACKET_SET_RPM = 3,
-    CAN_PACKET_SET_POS = 4,
-    CAN_PACKET_SET_CURRENT_REL = 10,
-    CAN_PACKET_SET_CURRENT_BRAKE_REL = 11,
-    CAN_PACKET_SET_CURRENT_HANDBRAKE = 12,
-    CAN_PACKET_SET_CURRENT_HANDBRAKE_REL = 13,
-    CAN_PACKET_STATUS_1 = 9,
-    CAN_PACKET_STATUS_2 = 14,
-    CAN_PACKET_STATUS_3 = 15,
-    CAN_PACKET_STATUS_4 = 16,
-    CAN_PACKET_STATUS_5 = 27,
-    CAN_PACKET_STATUS_6 = 28,
-    CAN_PACKET_MAKE_ENUM_32_BITS = 0xFFFFFFFF,
-} CAN_PACKET_ID;
+void core_0_setup(void* params);
 
-void buffer_append_int16(uint8_t *buffer, int16_t number, int32_t *index);
-
-void buffer_append_int32(uint8_t *buffer, int32_t number, int32_t *index);
-
-void buffer_append_float16(uint8_t *buffer, float number, float scale, int32_t *index);
-
-void buffer_append_float32(uint8_t *buffer, float number, float scale, int32_t *index);
-
-void comm_can_set_duty(uint8_t controller_id, float duty);
-
-void comm_can_set_current(uint8_t controller_id, float current);
-
-void comm_can_set_current_off_delay(uint8_t controller_id, float current, float off_delay);
-
-void comm_can_set_current_brake(uint8_t controller_id, float current);
-
-void comm_can_set_rpm(uint8_t controller_id, float rpm);
-
-void comm_can_set_pos(uint8_t controller_id, float pos);
-
-void comm_can_set_current_rel(uint8_t controller_id, float current_rel);
-
-void comm_can_set_current_rel_off_delay(uint8_t controller_id, float current_rel, float off_delay);
-
-void comm_can_set_current_brake_rel(uint8_t controller_id, float current_rel);
-
-void comm_can_set_handbrake(uint8_t controller_id, float current);
-
-void comm_can_set_handbrake_rel(uint8_t controller_id, float current_rel);
-
-void comm_can_status_1(uint8_t controller_id);
-
-void comm_can_status_2(uint8_t controller_id);
-
-void comm_can_status_3(uint8_t controller_id);
-
-void comm_can_status_4(uint8_t controller_id);
-
-void comm_can_status_5(uint8_t controller_id);
-
-void comm_can_status_6(uint8_t controller_id);
-
-void print_raw_can_data(void* fasz);
+void put_message_in_buffer(void);
