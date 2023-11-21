eaeb82df27ed0108b984c016cdc2b09e4a63de2d
volubepossum <domj.roli@gmail.com>
2023-11-21 13:42:31 +0100

    add read

--- src/sd.cpp
+++ src/sd.cpp
@@ -1,6 +1,6 @@
 // sd.cpp
 // inspiration from https://rydepier.wordpress.com/2015/08/07/using-an-sd-card-reader-
-//to-store-and-retrieve-data-with-arduino/
+// to-store-and-retrieve-data-with-arduino/
 
 #include "sd.hpp"
 #include <SPI.h>
@@ -12,37 +12,44 @@
 // MISO  = 19;
 // SCK   = 18;
 
-String dataString =""; // holds the data to be written to the SD card
-String aiString ="";
-//std::string myDataString = "";
-char dataLogFileName[12] = {'d','a','t','a','0','0','0','0','.','c','s','v'};
+String dataString = ""; // holds the data to be written to the SD card
+String aiString = "";
+// std::string myDataString = "";
+char dataLogFileName[12] = {'d', 'a', 't', 'a', '0', '0', '0', '0', '.', 'c', 's', 'v'};
 
 // array
-//float escData[12]; // duty, currentM, erpm
+// float escData[12]; // duty, currentM, erpm
 float sdLoggingFloat[LOG_LENGTH]; // duty, currentM, erpm, tFET, tMot, tacho, Vbatt, Ibatt
 
-void FillLogWithZeros(){
-    for (int i=0; i < LOG_LENGTH; i++){
-        sdLoggingFloat[i]=0.00;
+void FillLogWithZeros()
+{
+    for (int i = 0; i < LOG_LENGTH; i++)
+    {
+        sdLoggingFloat[i] = 0.00;
     }
 }
 
-void LogAppendValues(){
-    for(int i=0; i<LOG_LENGTH-1; i++){
+void LogAppendValues()
+{
+    for (int i = 0; i < LOG_LENGTH - 1; i++)
+    {
         dataString += String(sdLoggingFloat[i]) + ",";
     }
-    dataString += String(sdLoggingFloat[LOG_LENGTH-1]);
+    dataString += String(sdLoggingFloat[LOG_LENGTH - 1]);
 }
 
-char* findDataLogFileName(){
-    for(int i=0; i<10000; i++){
+char *findDataLogFileName()
+{
+    for (int i = 0; i < 10000; i++)
+    {
         dataLogFileName[7] = '0' + i % 10;
         dataLogFileName[6] = '0' + (i % 100 - i % 10) / 10;
         dataLogFileName[5] = '0' + (i % 1000 - i % 100) / 100;
         dataLogFileName[4] = '0' + (i % 10000 - i % 1000) / 1000;
 
-        if(!SD.exists(dataLogFileName)){
-            return dataLogFileName; 
+        if (!SD.exists(dataLogFileName))
+        {
+            return dataLogFileName;
         }
     }
     return dataLogFileName;
@@ -50,51 +57,24 @@ char* findDataLogFileName(){
 
 File sensorDataLog;
 
-void saveDataLog(){
-if(SD.exists(dataLogFileName)){ // check the card is still there
-    // now append new data file
-    sensorDataLog = SD.open(dataLogFileName, FILE_WRITE);
-    if (sensorDataLog){
-        sensorDataLog.println(dataString);
-        sensorDataLog.close(); // close the file
-    }
-}
-// else{
-//     Serial.println("Error writing to file !");
-// }
-}
-
-File aiParameter;
-
-void saveAiParameter(){
-    if(SD.exists("ai.csv")){ // check the card is still there
+void saveDataLog()
+{
+    if (SD.exists(dataLogFileName))
+    { // check the card is still there
         // now append new data file
-        aiParameter = SD.open("ai.csv", FILE_WRITE);
-        if (aiParameter){
-            aiParameter.println(aiString);
-            aiParameter.close(); // close the file
+        sensorDataLog = SD.open(dataLogFileName, FILE_WRITE);
+        if (sensorDataLog)
+        {
+            sensorDataLog.println(dataString);
+            sensorDataLog.close(); // close the file
         }
     }
     // else{
     //     Serial.println("Error writing to file !");
     // }
 }
-std::string readAiParameter(){
-    if (SD.exists("ai.csv")) {
 
-        if (aiParameter) {
-            std::string result = {char(aiParameter.read())}; // really not sure if it works
-            aiParameter.close();
-            return result;
-        } else {
-            return "failed to read";
-        }
-        
-    }
-}
-
-
-
+File aiParameter;
 
 // void setup()
 // {
@@ -119,4 +99,19 @@ std::string readAiParameter(){
 //     dataString = String(sensorReading1) + "," + String(sensorReading2) + "," + String(sensorReading3); // convert to CSV
 //     saveData(); // save to SD card
 //     delay(60000); // delay before next write to SD Card, adjust as required
+// }
+
+// void readData(){
+//     // read from the SD card and display on the serial monitor
+//     sensorDataLog = SD.open(dataLogFileName);
+//     if (sensorDataLog){
+//         Serial.println("Reading from file:");
+//         while (sensorDataLog.available()){
+//             Serial.write(sensorDataLog.read());
+//         }
+//         sensorDataLog.close(); // close the file
+//     }
+//     else{
+//         Serial.println("Error opening file !");
+//     }
 // }
